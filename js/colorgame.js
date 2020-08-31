8cae20d3c0bed8943a5224c0081580ff41b68157
Florian Hergenhahn <management@flouet.com>
2020-08-31 09:55:23 +0200

    Improving code efficency, adding comments, general code cleanup

--- js/colorgame.js
+++ js/colorgame.js
@@ -11,77 +11,117 @@ var mode=document.querySelectorAll(".difficult")
 
 init();
 
-
-function init(){
+/*
+This function initializes the game
+*/
+function init() {
   //Game initiation
   Display.textContent=picked;
-  for (var i = 0;i<squares.length;i++){
+  
+  // Iterate over the squares and add a click listener
+  for (let i = 0; i < squares.length; i++){
     squares[i].addEventListener("click", function () {
-      if(this.style.backgroundColor === picked){
+      // Check if the background color equals to the picked color
+      if(this.style.backgroundColor === picked) {
+        // Display the win
         check.textContent="Correct";
         reset.textContent="Play again?"
         swap(squares);
         back.style.backgroundColor = this.style.backgroundColor;
-      }
-      else{
+      } else {
+        // Display the loss
         this.style.backgroundColor = "#232323"
         check.textContent="fail!";
       }
-  });
-}
-  //mode event
-  for (var i=0;i<mode.length;i++){
-  mode[i].addEventListener("click", function(){
-    mode[0].classList.remove("active");
-    mode[1].classList.remove("active");
-    this.classList.add("active");
-    this.textContent==="EASY" ? diff=3: diff=6;
-    resetting();
-  })
-}
-  //reset button
+    });
+  }
+  
+  // Iterate over the different modes and add a click listener
+  for (let i=0; i < mode.length; i++){
+    mode[i].addEventListener("click", function() {
+      mode[0].classList.remove("active");
+      mode[1].classList.remove("active");
+      this.classList.add("active");
+      // When clicked, the difficulty will be switched to easy
+      this.textContent === "EASY" ? diff=3: diff=6;
+      resetting();
+    });
+  }
+  
+  //Adding event listener for the reset button
   reset.addEventListener("click", function () {
-  resetting();
-})
+    // When the buttion is clicked it will reset the game
+    resetting();
+  });
+  
   resetting();
 }
+
+/*
+This function will reset the game so a new round can be played
+*/
 function resetting(){
+  // Genreate random colors based on the difficulty
   colors=genCol(diff);
+  // Pick a random color from the generated colors
   picked=pickColor();
+  
+  // Displays the picked color
   Display.textContent=picked;
   reset.textContent="New colors"
   check.textContent="";
   back.style.backgroundColor="steelblue";
+  
+  // Iterate over the squares and set the background color to the respecive genreated color
   for (var i = 0; i < squares.length; i++) {
-    if(colors[i]){
+    // Checks if the color array is not null at the current index
+    if(colors[i]) {
       squares[i].style.display="block";
       squares[i].style.backgroundColor=colors[i];
-    }
-    else{
+    } else {
+      // If there is no color avalible at the current index, the square will not be displayed
       squares[i].style.display="none";
     }
   }
 }
-function swap(any){
-  for (var i = 0;i < any.length;i++){
+
+/*
+This function will swap the backgrond color of the tiles, with the header background color
+*/
+function swap(any) {
+  for (let i = 0; i < any.length; i++){
     any[i].style.backgroundColor = picked;
   }
   head.style.backgroundColor = picked;
 }
+
+/*
+This function will pick a color from the  
+*/
 function pickColor(){
   var X= Math.floor(Math.random()* colors.length);
-  return colors[X];
+  return this.colors[X];
 }
-function genCol(num){
-  var arr=[];
-  for(var c=0;c<num;c++){
+
+/*
+This function will generate a given amount of colors in from of an array 
+*/
+function genCol(num) {
+  const arr = [];
+  
+  for(let c=0; c < num; c++){
     arr.push(ranCol());
   }
+  
   return arr;
 }
+
+/*
+This function will generate a random color
+*/
 function ranCol(){
-    var r=Math.floor(Math.random()*256);
-    var g=Math.floor(Math.random()*256);
-    var b=Math.floor(Math.random()*256);
-    return "rgb(" + r +", " + g + ", " + b + ")";
+  const r = Math.floor(Math.random()*256);
+  const g = Math.floor(Math.random()*256);
+  const b = Math.floor(Math.random()*256);
+  return "rgb(" + r +", " + g + ", " + b + ")";
 }
